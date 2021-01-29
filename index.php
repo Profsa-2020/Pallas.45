9bad4a39408495426d0c317d83b3d23a42534332
Paulo R F Souza <diretoria@profsa.com.br>
2021-01-29 11:02:09 -0300

    Primeiro envio - Base

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="pt_br">
+
+<head>
+     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+     <meta name="description" content="Profsa Informática - UpLoad de dados e Análise de Investimentos" />
+     <meta name="author" content="Paulo Rogério Souza" />
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+     <link href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" type="text/css" />
+     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet" type="text/css" />
+
+     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css">
+
+	<link rel="shortcut icon" href="http://www.profsa.com.br/pallas45/img/favicon.png" />
+     
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+     <link href="css/pallas44.css" rel="stylesheet" type="text/css" media="screen" />
+     <title>Login - Análise de Investimentos - Profsa Informátda Ltda</title>
+</head>
